79c8e6b6b5dac445594efb22fd06f89a661ceb4a
guild13 <63346560+guild13@users.noreply.github.com>
2022-03-29 09:04:38 +0100

    Last Q of task_2 and start of task_4

---
+++ task_4/data_cleaning_scripts/data_cleaning.R
@@ -0,0 +1,12 @@
+# Loading libraries and Data
+
+library(tidyverse)
+library(here)
+library(janitor)
+library(readxl)
+
+candy2015 <- read_excel("task_4/raw_data/boing-boing-candy-2015.xlsx")
+
+candy2016 <- read_excel("task_4/raw_data/boing-boing-candy-2016.xlsx")
+
+candy2017 <- read_excel("task_4/raw_data/boing-boing-candy-2017.xlsx")
